18f47509815d74ba7584fed00358b5355d76ba42
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-25 18:19:40 -0500

    Add data to seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_175300) do
+ActiveRecord::Schema.define(version: 2021_03_25_174927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
-  create_table "bookstores", force: :cascade do |t|
-    t.string "name"
-    t.boolean "has_coffee_shop"
-    t.integer "days_open_per_week"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "employees", force: :cascade do |t|
-    t.integer "bookstore_id"
-    t.string "name"
-    t.boolean "employee_of_the_month"
-    t.integer "age"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_foreign_key "employees", "bookstores"
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -52,5 +33,23 @@ ActiveRecord::Schema.define(version: 2021_03_24_175300) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "bookstores", force: :cascade do |t|
+    t.string "name"
+    t.boolean "has_coffee_shop"
+    t.integer "days_open_per_week"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.integer "bookstore_id"
+    t.string "name"
+    t.boolean "employee_of_the_month"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "books", "authors"
+  add_foreign_key "employees", "bookstores"
 end
