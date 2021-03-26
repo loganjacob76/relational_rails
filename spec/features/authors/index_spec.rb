cd5b674cff76d0fef6148ff0cd28e0b4cef001d4
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-26 16:36:11 -0500

    Update test for user story 6

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe "authors index page", type: :feature do
     expect(page).to have_content(author_2.name)
   end
 
+  it "can see date and time when author was created" do
+    author_1 = Author.create(name: "Stephen King", best_selling_author: true, year_first_published: 1974)
+    author_2 = Author.create(name: 'Cal Newport', best_selling_author: true, year_first_published: 2005)
+
+    visit "/authors"
+
+    expect(page).to have_content(author_1.created_at)
+    expect(page).to have_content(author_2.created_at)
+  end
+
   it "can see link to all books page" do
     visit "/authors"
 
