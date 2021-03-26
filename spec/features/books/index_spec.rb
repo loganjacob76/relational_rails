c2f1db533565f64f608d51f43d30fd1c0cebcdf4
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-25 19:56:26 -0500

    Create test for user story 3

---
+++ spec/features/books/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "books index page", type: :feature do
+  it "can see names of all books" do
+    author_1 = Author.create(name: "Stephen King", best_selling_author: true, year_first_published: 1974)
+    author_2 = Author.create(name: 'Cal Newport', best_selling_author: true, year_first_published: 2005)
+    book_1 = Book.create!(name: 'Deep Work', fiction: false, number_of_pages: 304, author: author_1)
+    book_2 = Book.create!(name: 'Digital Minimalism', fiction: false, number_of_pages: 302, author: author_2)
+
+    visit "/books"
+
+    expect(page).to have_content(book_1.name)
+    expect(page).to have_content(book_2.name)
+  end
+end
