fd41ff2fb4efd6e9f38f29e781f36ed910d8c80d
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-03-28 18:30:31 -0500

    Create and pass tests for User Story 11

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,11 +3,28 @@ class AuthorsController < ApplicationController
     @authors = Author.all.order(:created_at)
   end
 
+  def new
+  end
+
+  def create
+    # Author.create(author_params)
+    @author = Author.create(name: params[:name], best_selling_author: params[:best_selling_author], year_first_published: params[:year_first_published])
+    redirect_to '/authors'
+  end
+
+  # private
+  # def author_params
+  #   params.permit(:name, :best_selling_author, :year_first_published)
+  # end
+
   def show
     @author = Author.find(params[:id])
   end
 
   def relationship
     @authors_books = Author.find(params[:id]).books
+  end
+
+  def edit
   end
 end
